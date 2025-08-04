ee3e50aa5e15c18154a0d6c384a418acbd01a53c
Godfreychangaya254 <changayagodfrey8@gmail.com>
2025-08-04 22:06:11 +0300

    initial commit

---
+++ BasicCalculator/calculator.py
@@ -0,0 +1,25 @@
+# Basic Calculator Program
+
+# Get user input
+num1 = float(input("Enter the first number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+
+# Perform calculation based on operator
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error: Division by zero is not allowed."
+else:
+    result = "Error: Invalid operator."
+
+# Print the result
+print(f"{num1} {operator} {num2} = {result}")
+
